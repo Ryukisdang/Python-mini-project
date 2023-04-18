e65e79319e9122ef2aff55a74ad59a4540d860e3
RyukazeIsDangT <117620379+Ryukisdang@users.noreply.github.com>
2023-04-18 17:14:56 +0530

    Area of rectangle in float value.py

---
+++ Area of rectangle in float value.py
@@ -0,0 +1,4 @@
+w = float(input("Enter the width:"))
+h = float(input("Enter the height:"))
+area = w * h
+print("This is the result: ",area)
---
+++ pi = 3.py
@@ -0,0 +1,5 @@
+pi = 3.14
+r = float(input("Enter the radius: "))
+h = float(input("Enter the height: "))
+volume = pi *(r**2) * h
+print("volume of cylinder: ",volume)
